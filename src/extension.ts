b46a62cda3371c24515781da3d2f9c8674e18ee5
halfenif <halfenif@gmail.com>
2024-06-10 13:32:55 +0900

    1st package

--- src/extension.ts
+++ src/extension.ts
@@ -12,8 +12,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('new-file-for-jekyll.newfile', async (uri: vscode.Uri) => {
 
-		vscode.window.showInformationMessage(`Start`);
-
 		// Check if the selected resource is a folder
 		if (uri && uri.scheme === 'file') {
 			//vscode.window.showInformationMessage(`Folder selected: ${uri.fsPath}`);
@@ -22,36 +20,57 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		  const currentDate = new Date();
-		  let strYear  = currentDate.getFullYear();
-		  let strMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-		  let strDay   = currentDate.getDate().toString().padStart(2, "0");
-		  let strHour   = currentDate.getHours().toString().padStart(2, "0");
-		  let strMinute   = currentDate.getMinutes().toString().padStart(2, "0");
-		  let strSecond   = currentDate.getSeconds().toString().padStart(2, "0");
+		const currentDate = new Date();
+		let strYear  = currentDate.getFullYear();
+		let strMonth = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+		let strDay   = currentDate.getDate().toString().padStart(2, "0");
+		let strHour   = currentDate.getHours().toString().padStart(2, "0");
+		let strMinute   = currentDate.getMinutes().toString().padStart(2, "0");
+		let strSecond   = currentDate.getSeconds().toString().padStart(2, "0");
 		  
-		  let fileName = `${strYear}-${strMonth}-${strDay}_${strHour}${strMinute}${strSecond}.md`;
-		  let filePath = uri.fsPath + "/" + fileName;
+		let fileName = `${strYear}-${strMonth}-${strDay}_${strHour}${strMinute}${strSecond}.md`;
+		let filePath = uri.fsPath + "/" + fileName;
 
-		  let strUUID = uuidv4();
+		let strUUID = uuidv4();
+
+		let strBaseName = path.basename(uri.fsPath);
+
+		//-------------------------
+		let templateContent = `---
+layout: post
+title: 
+date: ${strYear}-${strMonth}-${strDay} ${strHour}:${strMinute}:${strSecond} +0900
+categories: ${strBaseName}
+tags: ${strBaseName}
+description: 
+---
+
+# Link Sample
+[Link Name](https://url)
+
+# Image Sample
+| ![1](/assets/images/${strYear}-${strMonth}-${strDay}-01-01.png) |
+| :------------------------------------: |
+|   Image Desc.    |
 
 
-		  let templateContent = `UUID=${strUUID}`;
+![Read Count]({%- include count_url.html -%}${strUUID})
+`;
 
-		  try {
-			fs.writeFileSync(filePath, templateContent);
-			
-		  } catch(error) {
-			let message = "Unknown Error";
-			if (error instanceof Error) {
-				message = error.message;			
-				vscode.window.showErrorMessage('Failed to write to file: ' + message);
-				return;
-			}
-		  }
+		try {
+		fs.writeFileSync(filePath, templateContent);
+		
+		} catch(error) {
+		let message = "Unknown Error";
+		if (error instanceof Error) {
+			message = error.message;			
+			vscode.window.showErrorMessage('Failed to write to file: ' + message);
+			return;
+		}
+		}
 
-		  const document = await vscode.workspace.openTextDocument(filePath);
-		  await vscode.window.showTextDocument(document);
+		const document = await vscode.workspace.openTextDocument(filePath);
+		await vscode.window.showTextDocument(document);
 
 		  //Refresh
 		//   vscode.commands.executeCommand('workbench.files.action.refreshFilesExplorer')
